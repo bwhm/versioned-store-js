75e8add7484adf2ee93846c0c13711cdd359120f
bwhm <martin@jsgenesis.com>
2020-02-04 18:56:01 +0100

    add scripts that should be run at launch (and batching in general)

---
+++ src/scripts/add-schema-support-to-entity-from-json-schema-batch.ts
@@ -0,0 +1,173 @@
+import { Substrate } from "../cli/substrate";
+import { prettyEntity } from '../cli/printers';
+import { checkForDuplicateExistingClassNames } from "../cli/checks";
+import { Credential } from '@joystream/types/lib/versioned-store/permissions/credentials';
+import { bool, u32, Option, Vec } from '@polkadot/types';
+import { OperationType} from '@joystream/types/lib/versioned-store/permissions/batching/operation-types';
+import { Operation } from '@joystream/types/lib/versioned-store/permissions/batching/';
+import ClassId from '@joystream/types/lib/versioned-store/ClassId';
+import { ParametrizedEntity } from '@joystream/types/lib/versioned-store/permissions/batching/parametrized-entity';
+import { ParametrizedPropertyValue } from '@joystream/types/lib/versioned-store/permissions/batching/parametrized-property-value';
+import ParametrizedClassPropertyValue from '@joystream/types/lib/versioned-store/permissions/batching/ParametrizedClassPropertyValue';
+import { transformPropertyValueToEnum } from '../transformPropertyValueToEnumValue'
+import { transformAddSchemaSupportToEntity } from '../transformAddSchemaSupportToEntity'
+import { PropertyByNameMap, AddSchemaSupportToEntityInputType, AddSchemaSupportToEntityOutputType, PropertyValueInputType } from '../types';
+import { PropertyValueEnumValue } from '@joystream/types/lib/versioned-store/PropertyValue';
+
+import entityJsons = require('../inputs/entity-values/add-new-schema-support-json-schemas/index.js');
+import EntityId from '@joystream/types/lib/versioned-store/EntityId';
+import PropertyTypeName from '@joystream/types/lib/versioned-store/PropertyTypeName';
+
+const CREDENTIAL_ONE = new u32(1);
+/*
+const CLASS_PERMISSIONS = new ClassPermissions({
+  entity_permissions: new EntityPermissions({
+    update: new CredentialSet([CREDENTIAL_ONE]),
+    maintainer_has_all_permissions: new bool(true),
+  }),
+  entities_can_be_created: new bool(true),
+  add_schemas: new CredentialSet([CREDENTIAL_ONE]),
+  create_entities: new CredentialSet([CREDENTIAL_ONE]),
+  reference_constraint: new ReferenceConstraint({'NoConstraint': new NoConstraint()}),
+  admins: new CredentialSet([]),
+  last_permissions_update: new u32(0), // BlockNumber
+});
+*/
+
+
+const classNamesInput = process.argv[2] as string
+console.log(classNamesInput)
+
+const classNameArray:string[] = classNamesInput.split(',')
+console.log(classNameArray)
+
+const entitityInput:AddSchemaSupportToEntityInputType[][] = []
+const entityIdsAsNumbers:number[] = []
+for (let i=0; i<classNameArray.length; i++) {
+  let entitityInputClass:AddSchemaSupportToEntityInputType[] = []
+  for (let n=0; n<entityJsons.default.length; n++ ) {
+    const index = entityJsons.default[n].classId.indexOf(classNameArray[i])
+    if (index != -1) {
+      for (let m=0; m<entityJsons.default[n].entities.length; m++) {
+        entityIdsAsNumbers.push(entityJsons.default[n].entities[m].entityId)
+      }
+      entitityInputClass = entityJsons.default[n].entities
+    }
+  }
+  entitityInput.push(entitityInputClass)
+}
+
+
+//TODO verify process.argv
+
+// async function
+async function main() {
+  const sub = new Substrate(); 
+  await sub.connect();
+  sub.setKeypair({
+    uri: '//Alice',
+    type: 'sr25519'
+  })
+
+  const classMap = await checkForDuplicateExistingClassNames(sub)
+  console.log('classes',classMap)
+  const classNameToIdMap = await sub.classNameToIdMap()
+  
+  const classIds = []
+  const classPropertyMaps: PropertyByNameMap[] = []
+  for (let i=0; i<classNameArray.length; i++) {
+    if (classNameToIdMap.has(classNameArray[i])) {
+      const classIdNumber = classNameToIdMap.get(classNameArray[i])
+      classIds.push(classIdNumber)
+      classPropertyMaps.push(await sub.getClassPropertyMap(new ClassId(classIdNumber)))
+    }
+  }
+  console.log("classPropertyMaps",classPropertyMaps)
+  
+
+  const enumsClassArray:PropertyValueEnumValue[][][] = []
+  const entityIdsInClassArray:EntityId[][] = []
+  const transformedAddSchemaSupportByClassArray:AddSchemaSupportToEntityOutputType[][] = []
+  for (let i=0; i<entitityInput.length; i++) {
+    const transformedAddSchemaSupportArray:AddSchemaSupportToEntityOutputType[] = []
+    const entityIdsArray:EntityId[] = []
+    const enumsArray:PropertyValueEnumValue[][] = []
+    for (let n=0; n<entitityInput[i].length; n++ ) {
+      const schema_with_values = entitityInput[i][n]
+      const entityEnum: PropertyValueEnumValue[] = []
+      const transformedAddSchemaSupport = transformAddSchemaSupportToEntity(schema_with_values,classPropertyMaps[i])
+      if (!transformedAddSchemaSupport.error) {
+        transformedAddSchemaSupportArray.push(transformedAddSchemaSupport.result)
+        if (entitityInput[i][n].propertyValues) {
+          for (let m=0; m<entitityInput[i][n].propertyValues.length; m++ ) {
+            //const classSchemaPropertyMaps:PropertyByNameMap = await sub.getClassSchemaPropertyMap(new ClassId(classIdNumber),schemaIdsArray[i]))
+            const propType = (classPropertyMaps[i].get(entitityInput[i][n].propertyValues[m].name)).type
+            const value = entitityInput[i][n].propertyValues[m].value
+            const propertyValueEnum:PropertyValueEnumValue = transformPropertyValueToEnum(propType,value)
+            entityEnum.push(propertyValueEnum)
+          }
+        } else {
+          const propType:PropertyTypeName = 'None'
+            const value:PropertyValueInputType = null
+            const propertyValueEnum:PropertyValueEnumValue = transformPropertyValueToEnum(propType,value)
+            entityEnum.push(propertyValueEnum)
+        }
+      }
+      enumsArray.push(entityEnum)
+      entityIdsArray.push(new EntityId(schema_with_values.entityId))
+    }
+    enumsClassArray.push(enumsArray)
+    entityIdsInClassArray.push(entityIdsArray)
+    transformedAddSchemaSupportByClassArray.push(transformedAddSchemaSupportArray)
+  }
+
+  // Batch Operations to execute in order..
+  let batch: Vec<Operation> = new Vec(Operation);
+
+    // First operation(s) - index 0 - entitityInput.length
+
+  for (let i=0; i<transformedAddSchemaSupportByClassArray.length; i++) {
+    for (let n=0; n<transformedAddSchemaSupportByClassArray[i].length; n++) {
+      const parametrizedPropertyValues:ParametrizedClassPropertyValue[] = []
+      for (let m=0; m<transformedAddSchemaSupportByClassArray[i][n].property_values.length; m++) {
+        parametrizedPropertyValues.push(new ParametrizedClassPropertyValue({
+          in_class_index: transformedAddSchemaSupportByClassArray[i][n].property_values[m].in_class_index,
+          value: ParametrizedPropertyValue.PropertyValue(enumsClassArray[i][n][m])
+        }))
+      }
+      batch.push(new Operation({
+        with_credential: new Option(Credential, CREDENTIAL_ONE),
+        as_entity_maintainer: new bool(true),
+        operation_type: OperationType.AddSchemaSupportToEntity(
+            ParametrizedEntity.ExistingEntity(new EntityId(entityIdsInClassArray[i][n])),
+            transformedAddSchemaSupportByClassArray[i][n].schema_id,
+            new Vec(ParametrizedClassPropertyValue, parametrizedPropertyValues)
+            )
+      }));
+    }
+  }
+  console.log(`Entities before updates:`)
+
+  for (let i=0; i<entityIdsAsNumbers.length; i++) {
+    const entity = await sub.getEntityById(entityIdsAsNumbers[i])
+      .catch(err => console.log(`Failed to get entity by id '${entityIdsAsNumbers[i]}'`, err));
+
+    const entityAsText = await prettyEntity(entity, sub)
+    console.log(`Entity by id`, entityIdsAsNumbers[i], entityAsText)
+  }
+
+  await sub.signTxAndSend(sub.vsTx.transaction(batch));
+
+  console.log(`All updated entitys:`)
+
+  for (let i=0; i<entityIdsAsNumbers.length; i++) {
+    const entity = await sub.getEntityById(entityIdsAsNumbers[i])
+      .catch(err => console.log(`Failed to get entity by id '${entityIdsAsNumbers[i]}'`, err));
+
+    const entityAsText = await prettyEntity(entity, sub)
+    console.log(`Entity by id`, entityIdsAsNumbers[i], entityAsText)
+  }
+sub.disconnect();
+}
+
+main()
---
+++ src/scripts/update-entity-properties-from-json-schema-batch.ts
@@ -0,0 +1,168 @@
+import { Substrate } from "../cli/substrate";
+import { prettyEntity } from '../cli/printers';
+import { checkForDuplicateExistingClassNames } from "../cli/checks";
+import { Credential } from '@joystream/types/lib/versioned-store/permissions/credentials';
+import { bool, u32, Option, Vec } from '@polkadot/types';
+import { OperationType} from '@joystream/types/lib/versioned-store/permissions/batching/operation-types';
+import { Operation } from '@joystream/types/lib/versioned-store/permissions/batching/';
+import ClassId from '@joystream/types/lib/versioned-store/ClassId';
+import EntityId from '@joystream/types/lib/versioned-store/EntityId';
+import { ParametrizedEntity } from '@joystream/types/lib/versioned-store/permissions/batching/parametrized-entity';
+import { ParametrizedPropertyValue } from '@joystream/types/lib/versioned-store/permissions/batching/parametrized-property-value';
+import ParametrizedClassPropertyValue from '@joystream/types/lib/versioned-store/permissions/batching/ParametrizedClassPropertyValue';
+import { transformPropertyValueToEnum } from '../transformPropertyValueToEnumValue'
+import { PropertyByNameMap, UpdateEntityPropertyValuesInputType } from '../types';
+import { PropertyValueEnumValue } from '@joystream/types/lib/versioned-store/PropertyValue';
+
+import entityJsons = require('../inputs/entity-values/update-entities/index.js');
+
+const CREDENTIAL_ONE = new u32(1);
+/*
+const CLASS_PERMISSIONS = new ClassPermissions({
+  entity_permissions: new EntityPermissions({
+    update: new CredentialSet([CREDENTIAL_ONE]),
+    maintainer_has_all_permissions: new bool(true),
+  }),
+  entities_can_be_created: new bool(true),
+  add_schemas: new CredentialSet([CREDENTIAL_ONE]),
+  create_entities: new CredentialSet([CREDENTIAL_ONE]),
+  reference_constraint: new ReferenceConstraint({'NoConstraint': new NoConstraint()}),
+  admins: new CredentialSet([]),
+  last_permissions_update: new u32(0), // BlockNumber
+});
+*/
+
+
+const classNamesInput = process.argv[2] as string
+//console.log(classNamesInput)
+
+
+const classNameArray:string[] = classNamesInput.split(',')
+console.log(classNameArray)
+
+
+//console.log("entityJsons",entityJsons)
+
+const entitityInput:UpdateEntityPropertyValuesInputType[][] = []
+const entityIdsAsNumbers:number[] = []
+for (let i=0; i<classNameArray.length; i++) {
+  let entitityInputClass:UpdateEntityPropertyValuesInputType[] = []
+  for (let n=0; n<entityJsons.default.length; n++ ) {
+    const index = entityJsons.default[n].classId.indexOf(classNameArray[i])
+    if (index != -1) {
+      for (let m=0; m<entityJsons.default[n].entities.length; m++) {
+        entitityInputClass = entityJsons.default[n].entities
+        entityIdsAsNumbers.push(entityJsons.default[n].entities[m].entityId)
+      }
+    }
+  }
+  entitityInput.push(entitityInputClass)
+}
+
+console.log("entitityInput",entitityInput)
+
+
+//TODO verify process.argv
+
+// async function
+async function main() {
+  const sub = new Substrate(); 
+  await sub.connect();
+  sub.setKeypair({
+    uri: '//Alice',
+    type: 'sr25519'
+  })
+
+  const classMap = await checkForDuplicateExistingClassNames(sub)
+  console.log('classes',classMap)
+  const classNameToIdMap = await sub.classNameToIdMap()
+  
+  const classIds = []
+  const classPropertyMaps: PropertyByNameMap[] = []
+  for (let i=0; i<classNameArray.length; i++) {
+    if (classNameToIdMap.has(classNameArray[i])) {
+      const classIdNumber = classNameToIdMap.get(classNameArray[i])
+      classIds.push(classIdNumber)
+      classPropertyMaps.push(await sub.getClassPropertyMap(new ClassId(classIdNumber)))
+    }
+  }
+ console.log("classPropertyMaps",classPropertyMaps)
+
+  const enumsClassArray:PropertyValueEnumValue[][][] = []
+  const entityIdsInClassArray:EntityId[][] = []
+  const inClassIndexClassArray:u32[][][] = []
+  for (let i=0; i<entitityInput.length; i++) {
+    const enumsArray:PropertyValueEnumValue[][] = []
+    const entityIdsArray:EntityId[] = []
+    const inClassIndexArray:u32[][] = []
+    for (let n=0; n<entitityInput[i].length; n++ ) {
+      const schema_with_values = entitityInput[i][n]
+      const entityEnum: PropertyValueEnumValue[] = []
+      const inClassIndexValueArray:u32[] = []
+      schema_with_values.entityId
+      for (let m=0; m<entitityInput[i][n].newPropertyValues.length; m++ ) {
+        const propType = (classPropertyMaps[i].get(entitityInput[i][n].newPropertyValues[m].name)).type
+        const value = entitityInput[i][n].newPropertyValues[m].value
+        const in_class_index = new u32((classPropertyMaps[i].get(entitityInput[i][n].newPropertyValues[m].name)).index)
+        const propertyValueEnum:PropertyValueEnumValue = transformPropertyValueToEnum(propType,value)
+        entityEnum.push(propertyValueEnum)
+        inClassIndexValueArray.push(in_class_index)
+      }
+      enumsArray.push(entityEnum)
+      entityIdsArray.push(new EntityId(schema_with_values.entityId))
+      inClassIndexArray.push(inClassIndexValueArray)
+    }
+    enumsClassArray.push(enumsArray)
+    entityIdsInClassArray.push(entityIdsArray)
+    inClassIndexClassArray.push(inClassIndexArray)
+  }
+  // Batch Operations to execute in order..
+  let batch: Vec<Operation> = new Vec(Operation);
+
+  for (let i=0; i<enumsClassArray.length; i++) {
+    for (let n=0; n<enumsClassArray[i].length; n++) {
+      const parametrizedPropertyValues:ParametrizedClassPropertyValue[] = []
+      for (let m=0; m<enumsClassArray[i][n].length; m++) {
+        parametrizedPropertyValues.push(new ParametrizedClassPropertyValue({
+          in_class_index: inClassIndexClassArray[i][n][m],
+          value: ParametrizedPropertyValue.PropertyValue(enumsClassArray[i][n][m])
+        }))
+      }
+      batch.push(new Operation({
+        with_credential: new Option(Credential, CREDENTIAL_ONE),
+        as_entity_maintainer: new bool(true),
+        operation_type: OperationType.UpdatePropertyValues(
+            ParametrizedEntity.ExistingEntity(entityIdsInClassArray[i][n]),
+            new Vec(ParametrizedClassPropertyValue, parametrizedPropertyValues)
+            )
+      }));
+    }
+  }
+
+  console.log(`Entities before updates:`)
+
+  for (let i=0; i<entityIdsAsNumbers.length; i++) {
+    const entity = await sub.getEntityById(entityIdsAsNumbers[i])
+      .catch(err => console.log(`Failed to get entity by id '${entityIdsAsNumbers[i]}'`, err));
+
+    const entityAsText = await prettyEntity(entity, sub)
+    console.log(`Entity by id`, entityIdsAsNumbers[i], entityAsText)
+  }
+
+  await sub.signTxAndSend(sub.vsTx.transaction(batch));
+
+  console.log(`All updated entitys:`)
+
+  for (let i=0; i<entityIdsAsNumbers.length; i++) {
+    const entity = await sub.getEntityById(entityIdsAsNumbers[i])
+      .catch(err => console.log(`Failed to get entity by id '${entityIdsAsNumbers[i]}'`, err));
+
+    const entityAsText = await prettyEntity(entity, sub)
+    console.log(`Entity by id`, entityIdsAsNumbers[i], entityAsText)
+  }
+
+
+sub.disconnect();
+}
+
+main()
