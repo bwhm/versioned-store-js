95555221b58d749305dcc9125b6a1ff35d779706
Alex Siman <aleksandr.siman@gmail.com>
2020-02-12 23:09:42 +0200

    Rename publication status values

--- src/inputs/entity-values/lists/publication-status.ts
+++ src/inputs/entity-values/lists/publication-status.ts
@@ -1,6 +1,6 @@
 import { PropertyValueInputType } from "../../../types/PropertyTypes";
 
 export const publicationStatus:PropertyValueInputType[] = [
-  "Unpublished",
-  "Published"
+  "Public",
+  "Unlisted"
   ];
